69ef02838b5a348e603768577b9849c55f220481
Qurence <shyposhartem.biz@gmail.com>
2025-05-03 00:11:35 +0300

    update modal.js

--- homework/js/modal.js
+++ homework/js/modal.js
@@ -2,7 +2,16 @@ const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const tooggleModal = () => modal.classList.toggle('is-hidden');
+const tooggleModal = () => {
+    modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('body-no-scroll'); 
+  };
 
 modalBtnOpen.addEventListener('click', tooggleModal);
 modalBtnClose.addEventListener('click', tooggleModal);
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      tooggleModal();
+    }
+  });
